b7c8a9ecb83f78594944b62e9d6db1f4c5d2b0e7
Abrar822 <abrarshekh822@gmail.com>
2025-07-11 20:20:01 +0530

    Update solar.js

--- solar.js
+++ solar.js
@@ -93,8 +93,6 @@ solarBtn.forEach((btn) => {
         if(getComputedStyle(exploreExosContainer).display === 'flex') {
             savedScrollPos = hiddenContainer.scrollTop;
         }
-        // To ensure it stays with no initial scroll
-        hiddenContainer.scrollTop = '0px';
         for(let section of sections) {
             if(getComputedStyle(section).display !== 'none' && !section.classList.contains('solar-system-container')) {
                 section.style.top = '1000px';
